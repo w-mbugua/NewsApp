fdcd0e561714d9948bf8c34c10b41e194e11dbba
JoyMbugua <wacumbugua@gmail.com>
2021-04-17 19:12:07 +0300

    created request file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}' # 'https://newsapi.org/v2/everything?q={}&from={}&sortBy=publishedAt&apiKey={}'
+    
 
 
 
