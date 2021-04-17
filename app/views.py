10c2bb9419cc74622422a6f32c318e1930126d88
JoyMbugua <wacumbugua@gmail.com>
2021-04-17 20:41:15 +0300

    created view function for source-based news

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from .request import get_news, get_sources
+from .request import get_news, get_sources, get_source_news
 
 #views
 @app.route('/')
@@ -15,4 +15,13 @@ def index():
     news_sources = get_sources()
     return render_template('index.html', title = title, popular=popular_news, top=top_headlines, sources=news_sources)
 
-
+@app.route('/<source>')
+def per_source(source):
+    """
+    view function that returns articles for each source
+    :param source: news source
+    :return: its articles
+    """
+    source_news = get_source_news(source)
+    title = source
+    return render_template('source.html', title=title, source=source_news)
